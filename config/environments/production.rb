4f5ece07ae71fd2a2fb801ae5f827276845a5297
ostadfreee <ostadfreee@yahoo.com>
2013-10-18 19:13:49 +0330

    Asset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,5 @@ MyApplication::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   config.assets.precompile += %w( hover_content.js )
+  coconfig.assets.initialize_on_precompile = false
 end
