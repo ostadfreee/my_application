0f073ee7f658b5c692d1b67a9b756037e0f29e1d
ostadfreee <ostadfreee@yahoo.com>
2013-11-08 18:33:53 +0330

    Gmail Email

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131016140139) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "authens", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -41,6 +44,10 @@ ActiveRecord::Schema.define(version: 20131016140139) do
     t.string   "image"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "reviews", force: true do |t|
